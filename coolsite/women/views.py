3096708df4fefd7646a28d7e9fe43db653fbb51d
Sp00n101 <92381196+Sp00n101@users.noreply.github.com>
2024-11-13 00:49:28 +0300

    Add files via upload

---
+++ coolsite/women/views.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect
+
+from .models import *
+
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
+
+
+def index(request): #HttpRequest
+    return HttpResponse("Страница приложения women.")
+
+def categories(request, catid):
+    if request.POST:
+        print(request.POST)
+
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
+
+def archive(request, year):
+    if int(year) > 2020:
+        return redirect('home', permanent=False)
+
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+    return HttpResponse(f"Отображение категории с id = {cat_id}")
